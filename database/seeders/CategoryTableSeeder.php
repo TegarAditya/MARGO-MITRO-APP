0db34518986dab2553c85fd05929c0ff3a58d2e8
Ramadhan Febri <rafeta02@student.uns.ac.id>
2022-07-01 07:59:54 +0700

    seeder category

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -22,54 +22,112 @@ class CategoryTableSeeder extends Seeder
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'BUKU'),
             ],
             [
-                'name'  => 'KERTAS',
-                'slug'  => SlugService::createSlug(Category::class, 'slug', 'KERTAS'),
-            ],
-            [
-                'name'  => 'BAHAN PEMBANTU',
-                'slug'  => SlugService::createSlug(Category::class, 'slug', 'BAHAN PEMBANTU'),
+                'name'  => 'BAHAN',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', 'BAHAN'),
             ]
         ];
         $subcats = [
             [
                 'name'  => 'SD K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'SD K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
             ],
             [
                 'name'  => 'MI K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'MI K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
             ],
             [
                 'name'  => 'SMP K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'SMP K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
             ],
             [
                 'name'  => 'MTS K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'MTS K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
             ],
             [
                 'name'  => 'SMA K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'SMA K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
             ],
             [
                 'name'  => 'SMK K13',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'SMK K13'),
+                'type'  => 'jenjang',
                 'parent_id' => 1,
+            ],
+            [
+                'name'  => '1',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '1'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '2',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '2'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '3',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '3'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '4',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '4'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '5',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '5'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '6',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '6'),
+                'type'  => 'kelas',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '64',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '64'),
+                'type'  => 'halaman',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => '96',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', '96'),
+                'type'  => 'halaman',
+                'parent_id' => 1,
+            ],
+            [
+                'name'  => 'KERTAS',
+                'slug'  => SlugService::createSlug(Category::class, 'slug', 'KERTAS'),
+                'type'  => 'bahan',
+                'parent_id' => 2,
             ],
             [
                 'name'  => 'PLATE',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'PLATE'),
-                'parent_id' => 3,
+                'type'  => 'bahan',
+                'parent_id' => 2,
             ],
             [
                 'name'  => 'CHECMICAL FLUID',
                 'slug'  => SlugService::createSlug(Category::class, 'slug', 'CHECMICAL FLUID'),
-                'parent_id' => 3,
+                'type'  => 'bahan',
+                'parent_id' => 2,
             ],
         ];
 
