3eb9ea05bb0beec6b4a7831784df94c732ae6298
vozaldi <vozaldi@gmail.com>
2022-10-21 07:14:40 +0700

    Template PO Dashboard (2)

--- resources/views/admin/productionOrders/dashboard.blade.php
+++ resources/views/admin/productionOrders/dashboard.blade.php
@@ -9,34 +9,71 @@
 
             {{-- List Order --}}
             <section class="border-top py-3" id="modelProduct">
-                <h6 class="mb-3">Daftar Sales Order</h6>
+                <h5 class="mb-3">Daftar Sales Order</h5>
 
                 @foreach ([1, 2, 3, 4] as $item)
                     <div class="card mb-2">
                         <div class="card-body px-3 py-2">
-                            <h6 class="text-sm product-name mb-0">
-                                Produk #{{ $loop->iteration }}
-                            </h6>
-
-                            <div class="row align-items-end align-self-center">
-                                <div class="col" style="max-width: 120px">
-                                    <p class="mb-0 text-sm">
-                                        Pesanan: 1
-                                    </p>
-
-                                    <p class="mb-0 text-sm">
-                                        Terkirim: 2
-                                    </p>
+                            <div class="row">
+                                <div class="col">
+                                    <h6 class="mb-0">
+                                        SO ORD/GENAP/MMJ/X/22/011
+                                    </h6>
                                 </div>
 
-                                <div class="col text-right" style="max-width: 240px">
-                                    <p class="text-sm mb-0">Harga</p>
-                                    <p class="m-0">@money(123456789)</p>
+                                <div class="col-auto">
+                                    <a href="#" class="btn btn-primary btn-sm">
+                                        Buat PO
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="row align-items-end align-self-center mt-3">
+                                <div class="col" style="margin-left: 5rem">
+                                    @foreach ([1, 2, 3, 4] as $product)
+                                        <h6 class="text-sm product-name mb-0">Product {{ $loop->iteration }}</h6>
+
+                                        <p class="mb-2 text-sm">
+                                            Jenjang: SMP
+                                        </p>
+            
+                                        <div class="row">
+                                            <div class="col-4">
+                                                <p class="mb-0 text-sm">
+                                                    Stock: 0
+                                                </p>
+            
+                                                <p class="mb-0 text-sm">
+                                                    HPP: Rp50.000
+                                                </p>
+                                            </div>
+            
+                                            <div class="col row align-items-end align-self-center">
+                                                <div class="col" style="max-width: 120px">
+                                                    <p class="mb-0 text-sm">
+                                                        Pesanan: 10000
+                                                    </p>
+            
+                                                    <p class="mb-0 text-sm">
+                                                        Terkirim: 0
+                                                    </p>
+                                                </div>
+            
+                                                <div class="col text-right" style="max-width: 240px">
+                                                    <p class="text-sm mb-0">Harga</p>
+                                                    <p class="m-0">Rp100.0000</p>
+                                                </div>
+            
+                                                <div class="col text-right">
+                                                    <p class="text-sm mb-0">Subtotal</p>
+                                                    <p class="m-0">Rp100.000.000</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
                                 </div>
 
-                                <div class="col text-right">
-                                    <p class="text-sm mb-0">Subtotal</p>
-                                    <p class="m-0">@money(123456789)</p>
+                                <div class="col-auto">
                                 </div>
                             </div>
                         </div>
@@ -50,21 +87,5 @@
 
 @push('styles')
 <style>
-.container-chart {
-    color: #1b1853
-}
-
-.card {
-    transition: 250ms ease-in-out box-shadow;
-    box-shadow: 0 .125rem .25rem rgba(0,0,0,.025),
-        0 .25rem .5rem rgba(0,0,0,0.025),
-        0 .5rem .5rem rgba(0,0,0,.025);
-}
-.card:hover {
-    cursor: pointer;
-    box-shadow: 0 .125rem .25rem rgba(0,0,0,.025),
-        0 .25rem .5rem rgba(15,15,65,0.0375),
-        0 .5rem .5rem rgba(25,25,155,.05);
-}
 </style>
 @endpush
